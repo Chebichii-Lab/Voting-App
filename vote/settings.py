caf9e79a7acb63f48d06c8fdf56d639717d0c734
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-26 23:27:23 +0300

    add home view function

--- vote/settings.py
+++ vote/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
+    'online.apps.OnlineConfig',
+    'cloudinary',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
