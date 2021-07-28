d6bbbe231347be818c70208acf6387c6f20ebe1d
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-07-28 21:44:17 +0300

    register admin fro the model classes

--- online/admin.py
+++ online/admin.py
@@ -1,3 +1,7 @@
+from online.models import Choice, Poll, Vote
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(Choice)
+admin.site.register(Vote)
